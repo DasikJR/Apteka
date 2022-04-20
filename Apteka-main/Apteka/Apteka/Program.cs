9bc2858a7cbc2b40769a71804849d4324dc8b44c
DasikJR <104075695+DasikJR@users.noreply.github.com>
2022-04-20 15:45:34 +0300

    Add files via upload

---
+++ Apteka-main/Apteka/Apteka/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+// Аптека
+public class Apteka
+{
+    // Название аптеки
+    public string Name { set; get; }
+
+    // Адрес
+    public string Location { set; get; }
+
+    // Лекарства
+    public List<Preparat> med;
+}
+
+// Лекарства
+public class Preparat
+{
+    // Название
+    public string PreparatName { set; get; }
+
+    // Цена
+    public float PreparatPrice { set; get; }
+   // public void Preparat(in int a)
+    
+}
+//Самое дорогое лекарство 
+public void MaxPrice()
+{
+    var res = float.MinValue;
+
+    foreach (var it in Preparat)
+    {
+        if (it.PreparatPrice < res) { res = it.PreparatPrice; }
+    }
+    return res;
+}
